51eaf19751a7b0aafc53b938c7528bbf8f0b830f
MD R UDDIN <47330880+md98uddin@users.noreply.github.com>
2021-02-15 09:42:49 -0500

    Update jsFunctions.js

--- jsFunctions.js
+++ jsFunctions.js
@@ -180,6 +180,107 @@ Object.grabValues = function (obj) {
   }
 };
 
+//sorts an object by its population attribute
+function sortObjects(arr){
+    let sorted=[];
+    for(let i=0; i<arr.length; i++){
+        currIndex=i+1;
+        for(let j=currIndex; j<arr.length; j++){
+            if(arr[i].population > arr[j].population){
+                let temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        sorted.push(arr[i]);
+    }
+
+    return sorted;
+}
+
+//checks if a nth degree number equals to sum of its individual digit power to nth
+function armstrong(num){
+    let arr=num.toString().split("");
+    let sum=0;
+    for(let i=0; i<arr.length;i++){
+        sum+=Math.pow(arr[i], arr.length);
+    }
+
+    console.log(sum);
+
+    return sum==num;
+}
+
+//checks if a brace string eg "(({{}}))" has even amount of braces
+function matchBrackets(str){
+    let map=new Map();
+
+    for(let i=0; i<str.length;i++){
+        if(map.has(str[i])){
+            map.set(str[i], map.get(str[i])+1)
+        } else {
+            map.set(str[i], 1);
+        }
+    }
+
+    let valid=true;
+    let braces=['[',']', '{','}', '(', ')'];
+
+    for(let i=0; i<braces.length ;i=i+2){
+        if(map.get(braces[i])!==map.get(braces[i+1]))
+            valid=false;
+    }
+
+    return valid;
+
+}
+
+//check if a string is read same backward and forward
+function palindrome(str){
+    let reverse=str.split("").reverse();
+    return reverse.join("")===str;
+}
+
+//check to see if two strings are anagrams
+function anagram(str1, str2){
+    if(str1.length!==str2.length) return false;
+    str1=str1.split("").sort()
+    str2=str2.split("").sort()
+    for(let i=0; i<str1.length;i++){
+        if(str1[i]!=str2[i])
+            return false;   
+    }
+
+    return true;
+}
+
+//most occuring character in a string
+function maxCharCounter(str){
+    let map=new Map();
+    if(str.length ===1) return str[0];
+
+    for(let i=0; i<str.length; i++){
+        if(map.has(str[i]))
+          map.set(str[i], (map.get(str[i]))+1);
+        else 
+          map.set(str[i], 1);
+    }
+
+    console.log(map);
+}
+
+//longest string in an array
+function longestString(arr){
+    if(arr.length===1) return arr[0];
+
+    let longest=arr[0];
+    for(let i=1; i<arr.length; i++)
+        if(arr[i].length > longest.length)
+            longest=arr[i];
+
+    return longest;
+}
+
 // TEST PASSED! //
 arr.myEach((element) => {
   console.log("element", element); // should print "element [i]" for every element
